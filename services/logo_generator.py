712ea9fc8fdb7295e0db449f13523b51a156372b
Norvik Alexian <norvik@ucraft.com>
2024-12-18 16:10:53 +0400

    created functions to generate the prompt and then pass that prompt to generate the logo

---
+++ services/logo_generator.py
@@ -0,0 +1,63 @@
+import uuid
+
+import vertexai
+from vertexai.preview.vision_models import ImageGenerationModel
+
+import config
+from common import openai_constants
+
+
+async def generate_prompt(business_name: str, style_type: str, keywords: list, colors: list) -> str:
+    """
+    Generate prompt for imagen3 model
+    :param business_name: Name of the business or brand.
+    :param style_type: Chosen style type.
+    :param keywords: List of keywords that describe user's business.
+    :param colors: Chosen color palette.
+    :return: Generated prompt
+    """
+    prompt_generator = await config.ASYNC_OPENAI_INSTANCE.chat.completions.create(
+        model=openai_constants.GPT_4O_MINI,
+        messages=[
+            {"role": "system", "content": openai_constants.PROMPT_GENERATOR.strip()},
+            {
+                "role": "user",
+                "content": f"Business Name: {business_name}, Style Type: {style_type}, Keywords: {keywords}, Colors: {colors}",
+            },
+        ],
+        temperature=0.5,
+    )
+
+    generated_prompt = prompt_generator.choices[0].message.content.strip()
+
+    return generated_prompt
+
+
+async def generate_logo(business_name: str, style_type: str, keywords: list, colors: list) -> str:
+    """
+    Generate logo
+    :param business_name: Name of the business or brand.
+    :param style_type: Chosen style type.
+    :param keywords: List of keywords that describe user's business.
+    :param colors: Chosen color palette.
+    :return: Generated logo in PNG format
+    """
+    prompt = await generate_prompt(business_name=business_name, style_type=style_type, keywords=keywords, colors=colors)
+    vertexai.init(project=config.PROJECT_ID, location=config.LOCATION)
+    imagen_model = ImageGenerationModel.from_pretrained(model_name=openai_constants.IMAGEN_3_MODEL)
+
+    image_generator = imagen_model.generate_images(
+        prompt=prompt,
+        aspect_ratio="1:1",
+        person_generation="allow_all",
+        guidance_scale=100.0,
+    )
+
+    image_generator.images[0]._pil_image.show()
+    generated_image_bytes = image_generator.images[0]._image_bytes
+    image_id = str(uuid.uuid4())[:5]
+
+    with open(f"logos/{image_id}.png", mode="wb") as image:
+        image.write(generated_image_bytes)
+
+    return "image saved successfully."
