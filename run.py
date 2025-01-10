8b7371713126609a8b801c4c7e072425a6d46f5d
Norvik Alexian <norvik@ucraft.com>
2025-01-10 15:40:30 +0400

    generate sample logos

--- run.py
+++ run.py
@@ -2,11 +2,13 @@ import asyncio
 
 from services import logo_generator
 
-business_name = "Norvik"
-style = "Character"
-keywords = ["burger", "cheese burger", "fast-food"]
-colors = ["#6f1d1b", "#708d81", "#8d0801"]
+business_name = "Pizzeria"
+style = "Badge"
+keywords = ["pizza", "pasta", "italian"]
+colors = ["#250902", "#640d14", "#ad2831"]
 
 logo = asyncio.run(
-    logo_generator.generate_logo(business_name=business_name, style_type=style, keywords=keywords, colors=colors)
+    logo_generator.generate_logo_set(
+        business_name=business_name, style_type=style, keywords=keywords, colors=colors, amount=1
+    )
 )
